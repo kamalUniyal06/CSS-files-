9a6b5a72d6dbb8b5a697709e12fdfd4b38e85973
kamalUniyal06 <kamaluniyal19@gmail.com>
2024-02-24 09:29:53 +0530

    first commit

---
+++ 15_variables_and _custom_properties/gourav.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    cout<<"Hello guys I am kamal"<<endl;
+    return 0;
+}
---
+++ 20_CSS grid/ram.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hii Kamal\n";
+    string name;
+    // puts(name);
+    // gets(name);
+    getline(cin,name);
+    cout<<name;
+
+    return 0;
+}
